cc9c62b6746238f08a04d8ccc2e73dd15cb23e81
Sarka Prokopova <sarka.prokopova.so@gmail.com>
2025-08-08 18:10:08 +0200

    clear app

--- app/app/+not-found.tsx
+++ app/app/+not-found.tsx
@@ -1,19 +1,11 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <View style={styles.container}>
+            <Text style={styles.text}>This page does not exist!</Text>
+      </View>
     </>
   );
 }
@@ -25,7 +17,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
-  link: {
+  text: {
     marginTop: 15,
     paddingVertical: 15,
   },
