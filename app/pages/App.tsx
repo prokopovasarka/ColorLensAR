a315ef2fd24c4564d495c9f0f1e07a6f76e92f7d
Sarka Prokopova <sarka.prokopova.so@gmail.com>
2025-08-13 19:10:13 +0200

    Viro and Navigation

---
+++ app/pages/App.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {
+  createStaticNavigation,
+  useNavigation,
+  NavigationContainer,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
+
+import CameraScreen from './Camera';
+
+type RootStackParamList = {
+  Home: undefined;
+  Camera: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function HomeScreen({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Menu</Text>
+      <Button onPress={() => navigation.navigate('Camera')}>
+        Camera
+      </Button>
+    </View>
+  );
+}
+
+function RootStack() {
+  return (
+    <Stack.Navigator initialRouteName="Home" 
+    screenOptions={{
+    headerStyle: { backgroundColor: 'tomato' },
+    }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Overview' }}
+      />
+      <Stack.Screen name="Camera" component={CameraScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f0f4f7',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '700',
+    marginBottom: 40,
+    color: '#333',
+  }
+});
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
---
+++ app/pages/Camera.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import {
+  ViroARScene,
+  ViroText,
+  ViroImage,
+  ViroTrackingStateConstants,
+  ViroARSceneNavigator,
+  ViroTrackingReason,
+} from "@reactvision/react-viro";
+
+const SceneAR = () => {
+  const [text, setText] = useState("Hello World!");
+
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText("AR Ready!");
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      setText("AR Unavailable");
+    } else {
+      setText("Initializing AR...");
+    }
+  }
+
+  return (
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+    </ViroARScene>
+  );
+};
+
+export default function CameraScreen() {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: SceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
